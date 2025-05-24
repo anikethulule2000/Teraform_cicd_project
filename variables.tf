d049a73e8d618c618aabe60e6f8fc1366b158fad
anikethulule2000 <99538737+anikethulule2000@users.noreply.github.com>
2025-05-24 17:22:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "key_name" {
+  description = "Name of the SSH key pair to use for EC2 instance"
+  type        = string
+}
+
+variable "github_token" {
+  description = "GitHub personal access token for webhook"
+  type        = string
+  sensitive   = true
+}
