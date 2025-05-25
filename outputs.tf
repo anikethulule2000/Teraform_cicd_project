e87832cb574562bd0191024558b1aeda93909316
anikethulule2000 <99538737+anikethulule2000@users.noreply.github.com>
2025-05-25 22:35:47 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,3 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
-}
-
-output "public_subnet_id" {
-  description = "ID of the public subnet"
-  value       = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  description = "ID of the private subnet"
-  value       = aws_subnet.private.id
-}
-
-output "web_server_public_ip" {
-  description = "Public IP address of the web server"
-  value       = aws_instance.web_server.public_ip
-}
-
 output "web_server_url" {
   description = "URL to access the web server"
   value       = "http://${aws_instance.web_server.public_ip}"
