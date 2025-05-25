0a8d1fb8cd856c37c762dd0b8566c4ea8ea32dd9
anikethulule2000 <99538737+anikethulule2000@users.noreply.github.com>
2025-05-25 10:49:34 +0530

    Update install-apache.sh

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -10,20 +10,4 @@ sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
-sudo bash -c "cat > /var/www/html/index.html" <<EOF
-<!DOCTYPE html>
-<html>
-<head>
-    <title>DevOps Assessment</title>
-    <style>
-        body { font-family: Arial, sans-serif; text-align: center; margin-top: 50px; }
-        h1 { color: #2c3e50; }
-        .info { background-color: #f8f9fa; padding: 20px; border-radius: 5px; 
-                display: inline-block; margin: 10px; }
-    </style>
-</head>
-<body>
-    <h1>Hello World </h1>
-</body>
-</html>
-EOF
+sudo echo "<h1> Hello World </h1>" > /var/www/html/index.html
